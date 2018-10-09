fca12857a9c1950bf25a74a84c4edb45cf461787
qiray <zotovyaa@mail.ru>
2018-10-09 15:26:30 +0300

    gitignore, draw example added

---
+++ draw.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func draw() {
+	rand.Seed(time.Now().UTC().UnixNano()) //Init random generator
+	width := 100
+	height := 100
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// Set color for each pixel.
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, color.RGBA{
+				uint8(rand.Intn(255)), //R
+				uint8(rand.Intn(255)), //G
+				uint8(rand.Intn(255)), //B
+				0xff})                 //Alpha
+		}
+	}
+
+	// Encode as PNG.
+	f, _ := os.Create("image.png")
+	png.Encode(f, img)
+}
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+//TODO: choose ANN to work with
+//TODO: read https://nplus1.ru/news/2018/01/19/attn-gan
+//TODO: read https://ru.wikipedia.org/wiki/%D0%93%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%BE-%D1%81%D0%BE%D1%81%D1%82%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D1%81%D0%B5%D1%82%D1%8C
+//https://habr.com/post/309508/ https://habr.com/post/348000/
+
+//TODO: read about drawing
 
 func main() {
-	fmt.Printf("It works!")
+	draw()
 }
