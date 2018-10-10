3514b001caf32f7b7146e2a5c7d4b61e120e6c59
qiray <zotovyaa@mail.ru>
2018-10-10 16:54:57 +0300

    Use TensorFlow

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 //TODO: choose ANN to work with
+//TODO: Tensorflow https://www.tensorflow.org/install/lang_go
 //TODO: read https://nplus1.ru/news/2018/01/19/attn-gan
 //TODO: read https://ru.wikipedia.org/wiki/%D0%93%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%BE-%D1%81%D0%BE%D1%81%D1%82%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D1%81%D0%B5%D1%82%D1%8C
 //http://robocraft.ru/blog/machinelearning/3693.html
