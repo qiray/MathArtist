647970a10132f35ebfc9818ca24751d037308b4d
qiray <zotovyaa@mail.ru>
2018-10-11 18:01:09 +0300

    TensorFlow test funcs

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 //TODO: choose ANN to work with
 //TODO: Tensorflow https://www.tensorflow.org/install/lang_go
+// https://neuralnet.info/2017/09/16/%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F-%D0%B4%D0%B5%D0%B2%D1%83%D1%88%D0%B5%D0%BA-%D0%B2-%D1%81%D1%82%D0%B8%D0%BB%D0%B5-%D0%B0%D0%BD%D0%B8%D0%BC%D0%B5/
+// https://make.girls.moe/#/
 //TODO: read https://nplus1.ru/news/2018/01/19/attn-gan
 //TODO: read https://ru.wikipedia.org/wiki/%D0%93%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%BE-%D1%81%D0%BE%D1%81%D1%82%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D1%81%D0%B5%D1%82%D1%8C
 //http://robocraft.ru/blog/machinelearning/3693.html
@@ -13,4 +15,5 @@ package main
 
 func main() {
 	draw()
+	tfTest()
 }
