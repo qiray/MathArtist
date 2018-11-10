bb46358ec97daec2a4e247f18be21ef42b724767
qiray <zotovyaa@mail.ru>
2018-11-10 13:34:32 +0300

    Name generator update

--- names_generator.py
+++ names_generator.py
@@ -3,10 +3,26 @@
 # Copyright (c) 2018, Yaroslav Zotov, https:github.com/qiray/
 # All rights reserved.
 
-# Some ideas and texts from http://ironigardinen.net/generatorer/art2/index.html
+# Text data from 
+# https://www.espressoenglish.net/100-common-nouns-in-english/
+# https://grammar.yourdictionary.com/parts-of-speech/adjectives/list-of-adjective-words.html
+# https://www.talkenglish.com/vocabulary/top-50-prepositions.aspx
+# http://ironigardinen.net/generatorer/art2/index.html
+
+# Some ideas from http://ironigardinen.net/generatorer/art2/index.html
 
 import random
-from names_data import *
+
+def file_to_list(path):
+    lines = []
+    with open(path) as f:
+        lines = f.read().splitlines()
+    return lines
+
+nouns = file_to_list("data/nouns.txt")
+adjectives = file_to_list("data/adjectives.txt")
+pronouns = file_to_list("data/pronouns.txt")
+prepositions = file_to_list("data/prepositions.txt")
 
 def adj_noun():
     return "%s %s" % (
@@ -37,14 +53,44 @@ def noun_prep_adj_noun():
         random.choice(adjectives),
         random.choice(nouns))
 
-generators = [adj_noun, pronoun_noun, pronoun_adj_noun, noun_prep_noun, noun_prep_adj_noun]
+def adj_noun_noun():
+    return "%s %s %s" % (
+        random.choice(adjectives),
+        random.choice(nouns),
+        random.choice(nouns))
+
+def pronoun_noun_noun():
+    return "%s %s %s" % (
+        random.choice(pronouns),
+        random.choice(nouns),
+        random.choice(nouns))
+
+def adj_noun_prep_adj_noun():
+    return "%s %s %s %s %s" % (
+        random.choice(adjectives),
+        random.choice(nouns),
+        random.choice(prepositions),
+        random.choice(adjectives),
+        random.choice(nouns))
+
+def pronoun_noun_prep_pronoun_noun():
+    return "%s %s %s %s %s" % (
+        random.choice(pronouns),
+        random.choice(nouns),
+        random.choice(prepositions),
+        random.choice(pronouns),
+        random.choice(nouns))
+
+def combined():
+    # TODO:
+    pass
+
+generators = [adj_noun, pronoun_noun, pronoun_adj_noun, noun_prep_noun, noun_prep_adj_noun,
+    adj_noun_noun, pronoun_noun_noun, adj_noun_prep_adj_noun, pronoun_noun_prep_pronoun_noun]
 
 def generate_name():
-    generator = random.choice(generators)
-    return generator()
+    result = random.choice(generators)()
+    result = result.lower().capitalize()
+    return result
 
-# TODO:
-# adjective + noun + noun
-# pronoun + noun + noun
-# adjective + noun + preposition + adjective + noun
-# cylced
+print (generate_name())
